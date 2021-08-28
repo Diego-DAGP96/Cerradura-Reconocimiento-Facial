d3ee44d167b2886727e910fc5acefa4b8c84ee8f
Diego Armando Godínez Padilla <60679334+Diego-DAGP96@users.noreply.github.com>
2021-08-28 18:09:49 -0500

    Se actualiza el código para capturar las fotos del nuevo usuario y preparar la data

--- FaceNet/UI_main.py
+++ FaceNet/UI_main.py
@@ -1,26 +1,67 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import subprocess
+from os import system
+
 
 class ejecuta_GUI(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("UserInterface.ui", self)
-        #self.actCerradura.setEnabled(False)
+
+        self.showRes.setText("Seleccione una opción.")
+
+        self.nombre.setVisible(False)
+        self.apellido.setVisible(False)
+        self.etiqNombre.setVisible(False)
+        self.etiqApellido.setVisible(False)
+        self.save.setVisible(False)
         self.registroUsr.clicked.connect(self.usrRegistrar)
 
     def usrRegistrar(self):
-        self.actCerradura.setEnabled(False)
-        self.showRes.setText("Activando Usuario. Por favor espere...")
-        subprocess.run("python camera.py --first_name Juan --last_name Camaney --maxFileNumber 10")
-        self.actCerradura.setEnabled(True)
+        self.showRes.setText("Por favor inserte su nombre y apellido")
 
+        self.registroUsr.setVisible(False)
+        self.actCerradura.setVisible(False)
+        self.save.setVisible(True)
+        self.nombre.setVisible(True)
+        self.apellido.setVisible(True)
+        self.etiqNombre.setVisible(True)
+        self.etiqApellido.setVisible(True)
+
+        self.save.clicked.connect(self.usrActivar)
+
+    def usrActivar(self):
+        self.showRes.setText("Activando Usuario. Por favor espere...")
+
+        nombre = self.nombre.toPlainText()
+        apellido = self.apellido.toPlainText()
+
+        if nombre == "":
+            self.showRes.setText("Favor de ingresar su nombre")
+        elif apellido == "":
+            self.showRes.setText("Favor de ingresar su apellido")
+        else:
+            self.showRes.setText("Activando Usuario. Por favor espere...")
+
+            comando = "python camera.py --first_name " + nombre + " --last_name " + apellido + " --maxFileNumber 10"
+            system(comando)
+            system("python prepare_data.py")
+
+            self.nombre.setPlainText("")
+            self.apellido.setPlainText("")
+
+            self.actCerradura.setVisible(True)
+            self.registroUsr.setVisible(True)
+            self.save.setVisible(False)
+            self.nombre.setVisible(False)
+            self.apellido.setVisible(False)
+            self.etiqNombre.setVisible(False)
+            self.etiqApellido.setVisible(False)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = ejecuta_GUI()
-    #ui = Ui_MainWindow()
-    #ui.setupUi(MainWindow)
+
     MainWindow.show()
     sys.exit(app.exec())
